89900952bb710812f656542e83387e4707153bd8
Marissa <mickles.marissa@gmail.com>
2023-02-04 11:22:14 -0500

    completed assignment

---
+++ rest-spread.js
@@ -0,0 +1,44 @@
+const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
+
+const findMin = (...args) => Math.min(...args);
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
+
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v * 2)];
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = items => {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+  }
+  
+/** Return a new array with every item in array1 and array2. */
+
+
+const extend = (array1, array2) => [...array1, ...array2];
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+
+const addKeyVal = (obj, key, val) => ({...obj, [key]: val});
+return newObj;
+
+
+/** Return a new object with a key removed. */
+
+let newObj = { ...obj }
+  delete newObj[key]
+  return newObj;
+
+
+/** Combine two objects and return a new object. */
+
+const combine = (obj1, obj2) => ({...obj1, ...obj2});
+
+
+/** Return a new object with a modified key and value. */
+
+const update = (obj, key, val) => ({...obj, [key]: val});
